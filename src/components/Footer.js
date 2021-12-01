169fc1dae6b6bdc808a57ad8dbb366e3f0921da9
Matthew Kiggen <28730291+mattkiggen@users.noreply.github.com>
2021-12-01 15:11:51 +0200

    add noreferrer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,25 @@ export default function Footer() {
     <footer className={styles.footer}>
       <p>
         Built with{' '}
-        <a href='https://create-react-app.dev/' target='_blank'>
+        <a
+          href='https://create-react-app.dev/'
+          target='_blank'
+          rel='noreferrer'>
           React
         </a>
         ,{' '}
-        <a href='https://react-leaflet.js.org/' target='_blank'>
+        <a
+          href='https://react-leaflet.js.org/'
+          target='_blank'
+          rel='noreferrer'>
           Leaflet
         </a>
         ,{' '}
-        <a href='https://www.mapbox.com/' target='_blank'>
+        <a href='https://www.mapbox.com/' target='_blank' rel='noreferrer'>
           MapBox
         </a>{' '}
         and{' '}
-        <a href='https://www.ipify.org/' target='_blank'>
+        <a href='https://www.ipify.org/' target='_blank' rel='noreferrer'>
           Ipify
         </a>
       </p>
